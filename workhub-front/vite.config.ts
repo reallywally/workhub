32d4aec97248ce005be3cbdeff510d56bc6f71e1
wally <goldenale5500@gmail.com>
2024-03-29 02:02:36 +0900

    프론트 설정으로 cqrs 해결
    - 근데 이거보단 백엔드에서 설정하는게 좋은거 같은데

--- workhub-front/vite.config.ts
+++ workhub-front/vite.config.ts
@@ -16,5 +16,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    // api 요청을 8080 포트로 프록시
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8080",
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
