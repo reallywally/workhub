90b7a4bb1da3915ea8453382c55686f67288e46a
wally <goldenale5500@gmail.com>
2024-06-03 23:48:30 +0900

    main.ts 수정중

--- workhub-front2/src/main.ts
+++ workhub-front2/src/main.ts
@@ -2,5 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router';
+import { createPinia } from 'pinia';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(createPinia());
+app.use(ElementPlus);
+app.mount('#app');
+
+
+// createApp(App).mount('#app')
