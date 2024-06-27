29ce7a58e7f8019b57de42ae8168a1461b231bc3
wally <goldenale5500@gmail.com>
2024-06-27 18:22:40 +0900

    광고주 등록 수정

--- workhub-front2/src/components/Advertiser/AdvertiserForm.vue
+++ workhub-front2/src/components/Advertiser/AdvertiserForm.vue
@@ -2,61 +2,57 @@
   <div>
     <h1> advertiser form</h1>
     <el-card>
-      <el-form @submit.prevent="handleSubmit" label-width="120px">
+      <el-form label-width="120px">
         <el-form-item label="사업자 등록 번호">
-          <el-input v-model="advertiser.businessNumber" required></el-input>
+          <el-input v-model="state.advertiserWrite.businessNumber" required></el-input>
         </el-form-item>
         <el-form-item label="법인명">
-          <el-input type="textarea" v-model="advertiser.businessName" required></el-input>
+          <el-input type="textarea" v-model="state.advertiserWrite.businessName" required></el-input>
         </el-form-item>
         <el-form-item label="광고주명(한글)">
-          <el-input type="textarea" v-model="advertiser.advertiserName" required></el-input>
+          <el-input type="textarea" v-model="state.advertiserWrite.advertiserName" required></el-input>
         </el-form-item>
         <el-form-item label="광고주명(영어)">
-          <el-input type="textarea" v-model="advertiser.advertiserNameEng" required></el-input>
+          <el-input type="textarea" v-model="state.advertiserWrite.advertiserNameEng" required></el-input>
         </el-form-item>
         <el-form-item label="전화번호">
-          <el-input type="textarea" v-model="advertiser.businessPhone"></el-input>
+          <el-input type="textarea" v-model="state.advertiserWrite.businessPhone"></el-input>
         </el-form-item>
         <el-form-item label="비고">
-          <el-input type="textarea" v-model="advertiser.note"></el-input>
+          <el-input type="textarea" v-model="state.advertiserWrite.note"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" native-type="submit">저장</el-button>
+          <el-button type="primary" @click="save()">저장</el-button>
         </el-form-item>
       </el-form>
     </el-card>
   </div>
 </template>
 
-<script>
-import { useAdvertiserStore } from '../../stores/advertiser';
-import {onMounted, ref} from 'vue';
-import { useRouter } from 'vue-router';
+<script setup lang="ts">
+import {reactive} from 'vue';
+import {useRouter} from 'vue-router';
+import {ElMessage} from 'element-plus'
+import {container} from 'tsyringe'
+import AdvertiserWrite from "@/entity/advertiser/AdvertiserWrite";
+import AdvertiserRepository from "@/repository/AdvertiserRepository";
+import HttpError from "@/http/HttpError";
 
-export default {
-  setup() {
-    const advertiserStore = useAdvertiserStore();
-    const router = useRouter();
-    const advertiser = ref({
-      businessNumber: '',
-      businessName: '',
-      advertiserName: '',
-      advertiserNameEng: '',
-      businessPhone: '',
-      note: '',
-    });
+const state = reactive({
+  advertiserWrite: new AdvertiserWrite(),
+})
 
+const ADVERTISER_REPOSITORY = container.resolve(AdvertiserRepository)
+const router = useRouter()
 
-    const handleSubmit = async () => {
-      await advertiserStore.createAdvertiser(advertiser.value);
-      router.push('/advertiser');
-    };
-
-    return {
-      advertiser,
-      handleSubmit
-    };
-  }
-};
+function save() {
+  ADVERTISER_REPOSITORY.write(state.advertiserWrite)
+      .then(() => {
+        ElMessage({type: 'success', message: '광고주 등록이 완료되었습니다.'})
+        router.replace('/advertiser')
+      })
+      .catch((e: HttpError) => {
+        ElMessage({type: 'error', message: e.getMessage()})
+      })
+}
 </script>
---
+++ workhub-front2/src/entity/advertiser/AdvertiserWrite.ts
@@ -0,0 +1,9 @@
+export default class AdvertiserWrite {
+    public businessNumber = ''
+    public businessName = ''
+    public advertiserName = ''
+    public advertiserNameEng
+    public businessPhone = ''
+    public note = ''
+
+}
