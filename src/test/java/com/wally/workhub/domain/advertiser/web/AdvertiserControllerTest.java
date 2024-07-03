448a4cd7862ae195e9fa658805fb1a86a23f58c0
wally <goldenale5500@gmail.com>
2024-07-03 14:02:43 +0900

    테스트 코드 테스트

--- src/test/java/com/wally/workhub/domain/advertiser/web/AdvertiserControllerTest.java
+++ src/test/java/com/wally/workhub/domain/advertiser/web/AdvertiserControllerTest.java
@@ -1,16 +1,19 @@
 package com.wally.workhub.domain.advertiser.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wally.workhub.config.TestMockUser;
+import com.wally.workhub.domain.advertiser.AdvertiserTestUtils;
+import com.wally.workhub.domain.advertiser.model.AdvertiserCreate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -20,17 +23,20 @@ class AdvertiserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    @TestMockUser
-    @DisplayName("광고주 목록 조회")
-    void test1() throws Exception {
+    @DisplayName("광고주 등록 테스트")
+    void testCreatAdvertiser() throws Exception {
         // given
+        AdvertiserCreate advertiserCreate = AdvertiserTestUtils.generateAdvertiserCreate();
 
-        // when
+        String json = objectMapper.writeValueAsString(advertiserCreate);
 
-        // then
-        mockMvc.perform(get("/advertisers?page=1&size=10")
-                        .contentType(APPLICATION_JSON))
+        mockMvc.perform(post("/advertisers")
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
                 .andExpect(status().isOk());
     }
 
